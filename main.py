be899eb790f6bc98ecc1a402fdb8963151e5c187
tiiedye <tiiedye@gmail.com>
2021-12-04 07:44:14 -0800

    Writing TODOs and the menu function

--- main.py
+++ main.py
@@ -1,2 +1,19 @@
 # Coffee Machine Project
+# TODO 1. Create menu options function
+# TODO 2. create functions to print report, order, refill resources, check payment, and turn off
+# TODO 3. create loop until user chooses off option
 
+from coffee_menu import MENU, resources
+
+
+def print_menu():
+    print("Welcome to the Coffee Maker, choose an option: ")
+    print("\tOrder Coffee [1]")
+    print("\tCheck Resources [2]")
+    print("\tTurn Off [3]")
+    option = input("\n Enter 1, 2, or 3: ")
+
+    while option != "1" or option != "2" or option != "3":
+        option = input("Invalid option, Enter 1, 2 or 3: ")
+
+    return option
